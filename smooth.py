4a472d42f160370cc8bb74b4d07c7a1b05cfe206
Complex and Sustainable Urban Networks (CSUN) Lab <csunlab@uic.edu>
2015-07-01 19:00:01 -0500

    Create smooth.py

---
+++ smooth.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Citation: N. Ahmad, S. Derrible, T. Eason, and H. Cabezas, “USING FISHER INFORMATION IN BIG DATA.” 
+(URL will be updated when finalized)
+"""
+import csv
+import matplotlib.pyplot as plt
+
+def FI_smooth(f_name,step,step_win):
+    out=open('FI.csv','rb')
+    data=csv.reader(out)
+    Data=[]
+        
+    for row in data:
+        Data.append(row)
+            
+    out.close()
+    
+    FI=[]
+    time=[]
+    
+    for row in Data:
+        FI.append(eval(row[-2]))
+        time.append(row[-1])
+        
+    FI_smth=[]
+    
+   
+    for i in range(step,len(FI)+step,step):
+        for j in range(i-step,i):
+            FI_smth.append(float(sum(FI[i-step:i]))/len(FI[i-step:i]))
+            
+    FI_smth=FI_smth[0:len(FI)]
+            
+
+    plt.plot(range(step_win,len(FI_smth)+step_win),FI_smth,'r',label='Smoothed')
+    plt.xlabel('Time Step')
+    plt.ylabel('Fisher Information')
+    plt.xticks(range(step_win,len(FI_smth)+step_win,5),
+                [time[i] for i in range(0,len(FI_smth),5)],rotation=45)
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(f_name+'FI_'+'.pdf')
+    plt.savefig(f_name+'FI_'+'.png',dpi=1000)
+    
+    
+    out=open('FI.csv','rb')
+    data=csv.reader(out)
+    Data=[]
+        
+    for row in data:
+        Data.append(row)
+            
+    out.close()
+    
+    for i in range(len(Data)):
+        Data[i].append(FI_smth[i])
+    
+    out=open("%s_FI.csv"%(f_name),"wb")
+    new=csv.writer(out)
+    
+
+    new.writerow(['Time_Step','FI','Smooth_FI'])
+    
+    for i in Data:
+        data_temp=[i[-2],i[-3],i[-1]]
+        new.writerow(data_temp)
+        
+    out.close()
+    
+
+
+
+    
