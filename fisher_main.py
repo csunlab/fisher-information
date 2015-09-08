65ee643068bf6e5e81d678ae48260d30138c9564
Complex and Sustainable Urban Networks (CSUN) Lab <csunlab@uic.edu>
2015-09-08 18:38:38 -0500

    Update fisher_main.py

--- fisher_main.py
+++ fisher_main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Citation: N. Ahmad, S. Derrible, T. Eason, and H. Cabezas, “USING FISHER INFORMATION IN BIG DATA.” 
-(URL will be updated when finalized)
+Citation: N. Ahmad, S. Derrible, T. Eason, and H. Cabezas, “Using Fisher Information In Big Data”, available on arXiv at http://arxiv.org/abs/1507.00389
+
 """
 
 
