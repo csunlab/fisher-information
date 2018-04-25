eecfa6c09b91c9fa81f94217b0acd1eca2c0a235
Complex and Sustainable Urban Networks (CSUN) Lab <csunlab@uic.edu>
2018-04-25 16:15:23 -0500

    Update and rename fisher_main.py to v1.00/fisher_main.py

--- v1.00/fisher_main.py
+++ v1.00/fisher_main.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
-'''
-Citation: N. Ahmad, S. Derrible, T. Eason, and H. Cabezas, “Using Fisher Information in Big Data”,
-available on arXiv at http://arxiv.org/abs/1507.00389
-'''
+"""
+N. Ahmad, S. Derrible, T. Eason, and H. Cabezas, 2016, “Using Fisher information to track stability in multivariate systems”,
+Royal Society Open Science, 3:160582, DOI: 10.1098/rsos.160582
+"""
 
 import datetime
 import os
-
 
 Time=datetime.datetime.now()
 
